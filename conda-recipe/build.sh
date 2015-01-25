c18128567fcae292e1e516b79fe2d10bd1323d62
David Froger <david.froger@inria.fr>
2015-01-25 16:44:50 +0100

    Fix install prefix

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -3,7 +3,7 @@ cd build
 
 # CMake detect ITK_DIR as $PREFIX/lib/cmake/ITK-4.7
 cmake \
-  -DCMAKE_INSTALL_PREFIX=$PREFX \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_PREFIX_PATH=$PREFIX \
   ..
 make -j$CPU_COUNT
