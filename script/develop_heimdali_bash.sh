8769a218c3c60311330c75d511c05e921a14eb0a
David Froger <david.froger@inria.fr>
2015-01-27 08:39:42 +0100

    A few on on develop Bash script

--- script/develop_heimdali_bash.sh
+++ script/develop_heimdali_bash.sh
@@ -3,6 +3,12 @@
 # Set up environnment variables for a development mode, during which things
 # are installed in heimdali/install.
 
+# This requires to have an conda enviromnent 'heimdali' with all dependencies
+# installed.
+
+# Note: this make LD_LIBRARY_PATH points to core libraries shipped with conda, and
+# could break a lot of usual command not shipped wiht conda (/usr/bin/vim, ...).
+
 # Check the file is sourced.
 if [ "${BASH_SOURCE[0]}" == "${0}" ]
 then 
@@ -29,5 +35,5 @@ LIB_DIR=$CONDA_ENV_PATH/lib
 export PATH=$BIN_DIR:$PATH
 export LD_LIBRARY_PATH=$LIB_DIR:$LD_LIBRARY_PATH
 
-echo "PATH has been prepended with               : $BIN_DIR"
+echo "PATH has been prepended with              : $BIN_DIR"
 echo "LD_LIBRARY_PATH has been prepended with   : $LIB_DIR"
