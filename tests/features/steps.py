15735f52d3daa7683b0bcea1861124431c8a6fcf
David Froger <david.froger@inria.fr>
2015-01-27 08:40:14 +0100

    FT: improve error handling when command failed

--- tests/features/steps.py
+++ tests/features/steps.py
@@ -3,6 +3,13 @@ from os.path import realpath, expandvars
 
 from lettuce import *
 
+def failed():
+    return world.returncode != 0 or world.stderr != ''
+
+def cmd_error():
+    raise AssertionError, "returncode is %r, stderr is %r, stdout is %r" % (
+            world.returncode, world.stdout, world.stderr)
+
 @step('I am in the heimdali data directory')
 def in_the_data_directory(step):
     world.cwd = world.data_dir
@@ -14,17 +21,20 @@ def in_the_directory(step,cwd):
 @step('I run the command: (.*)')
 def run_the_command(step,cmd):
     p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, cwd=world.cwd)
-    world.stdoutdata, world.stderrdata = p.communicate()
+    world.stdout, world.stderr = p.communicate()
     world.returncode = p.returncode
 
 @step('I see the line in standard output: (.*)')
 def i_see_the_line(step,line):
-    assert line in world.stdoutdata
+    if failed() or not line in world.stdout:
+        cmd_error()
 
 @step(u'Then I see the standard output:')
 def then_i_get_the_standard_output(step):
-    stdout = world.stdoutdata.strip()
-    assert step.multiline == stdout, "Got %r" % stdout
+    stdout = world.stdout.strip()
+    stderr = world.stderr.strip()
+    if failed() or step.multiline != stdout:
+        cmd_error()
 
 def factorial(number):
     return 1
