95f1e63b88c19f5b2f0dc97cbdc74a2645f3ffd4
David Froger <david.froger@inria.fr>
2015-02-11 15:54:21 +0100

    Add ad command

---
+++ cmd/ad.cxx
@@ -0,0 +1,80 @@
+#include <tclap/CmdLine.h>
+
+#include <itkImage.h>
+#include <itkAddImageFilter.h>
+#include <itkINRImageIOFactory.h>
+
+#include "heimdali/cmdreader.hxx"
+#include "heimdali/cmdwriter.hxx"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{ 
+
+TCLAP::CmdLine cmd("Add two images", ' ', "0.0.0");
+
+// -o output.h5
+TCLAP::ValueArg<string> output("o","output", 
+    "Output image, instead of standard output",false,"","FILENAME", cmd);
+
+// -streaming N
+TCLAP::ValueArg<int> streaming("s","streaming", "Number of lines to stream",
+    false, 0,"NUMBER_OF_LINES", cmd);
+
+// --force
+TCLAP::SwitchArg forceSwitch("v","verbose",
+    "ITK HDF5 IO operation are verboses.", cmd);
+
+// image0.h5
+TCLAP::UnlabeledValueArg<string> input0("image0", 
+    "First image.",true,"","IMAGE1",cmd);
+
+// image0.h5
+TCLAP::UnlabeledValueArg<string> input1("image1", 
+    "Second image.",true,"","IMAGE2",cmd);
+
+cmd.parse(argc,argv);
+
+// Put our INRimage reader in the list of readers ITK knows.
+itk::ObjectFactoryBase::RegisterFactory( itk::INRImageIOFactory::New() ); 
+
+// Image type.
+typedef float PixelType;
+const unsigned int Dimension = 3;
+typedef itk::VectorImage<PixelType, Dimension> ImageType;
+
+// Command line tool readers.
+typedef Heimdali::CmdReader<ImageType> ReaderType;
+ReaderType* reader1 = ReaderType::make_cmd_reader( streaming.getValue(),
+                                                   input0.getValue());
+
+ReaderType* reader2 = ReaderType::make_cmd_reader(streaming.getValue(),
+                                                  input1.getValue());
+// Command line tool writer.
+typedef Heimdali::CmdWriter<ImageType> WriterType;
+WriterType* cmdwriter = WriterType::make_cmd_writer(output.getValue());
+
+// Add filter.
+typedef itk::AddImageFilter <ImageType,ImageType> AddImageFilterType;
+AddImageFilterType::Pointer subtractFilter = AddImageFilterType::New ();
+
+size_t iregionmax = 1E+06;
+for (size_t iregion=0 ; iregion<iregionmax ; iregion++) {
+    // Read input.
+    reader1->next_iteration();
+    reader2->next_iteration();
+    if (reader1->is_complete()) break;
+
+    // Add images.
+    subtractFilter->SetInput1( reader1->GetOutput() );
+    subtractFilter->SetInput2( reader2->GetOutput() );
+
+    // Write output.
+    cmdwriter->Write( subtractFilter->GetOutput() );
+    cmdwriter->Update();
+}
+
+return 0;
+
+}
---
+++ tests/features/melg.feature
@@ -0,0 +1,10 @@
+Feature: The 'melg' command line tool.
+
+    Scenario: Invoke melg
+        When I run the command: melg --help
+        Then I see the line in standard output: Mix two images, testing on pixel values
+
+    Scenario: Override a image subregion
+        When I run the command: cp $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2_bi1.h5 melg_out.h5
+        When I run the command: melg -idz 2 -idy 2 -idx 2 -izi 1 -iyi 1 -ixi 1 $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2_two.h5 -izo 1 -iyo 1 -ixo 1 melg_out.h5
+        Then the HDF5 files melg_out.h5 and $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2_i1_j1_k1_x2_y2_z2_two.h5 are equal
