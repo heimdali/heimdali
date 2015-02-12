3b4b0d9bf5160244cf8d7c9503bcb84945e3b5fa
David Froger <david.froger@inria.fr>
2015-02-12 16:57:15 +0100

    Add lo command

---
+++ cmd/lo.cxx
@@ -0,0 +1,128 @@
+#include <tclap/CmdLine.h>
+
+#include <itkImage.h>
+#include <itkLogImageFilter.h>
+#include <itkVectorImageToImageAdaptor.h>
+#include <itkComposeImageFilter.h>
+#include <itkImageDuplicator.h>
+#include <itkINRImageIOFactory.h>
+
+#include "heimdali/cmdreader.hxx"
+#include "heimdali/cmdwriter.hxx"
+#include "heimdali/itkhelper.hxx"
+
+// Note: LogImageFilter do not work with VectorImage
+
+using namespace std;
+
+int main(int argc, char** argv)
+{ 
+
+try {
+
+TCLAP::CmdLine cmd("Compute logarithm of each pixel", ' ', "0.0.0");
+
+// -o output.h5
+TCLAP::ValueArg<string> output("o","image_out", 
+    "Output image, instead of standard output",false,"","FILENAME", cmd);
+
+// input.h5
+TCLAP::UnlabeledValueArg<string> input0("image_in", 
+    "First image.",true,"","IMAGE-IN",cmd);
+
+cmd.parse(argc,argv);
+
+// Put our INRimage reader in the list of readers ITK knows.
+itk::ObjectFactoryBase::RegisterFactory( itk::INRImageIOFactory::New() ); 
+
+// Image type.
+typedef itk::Image<Heimdali::PixelFloat, Heimdali::ImageDimension> ScalarImageType;
+typedef itk::VectorImage<Heimdali::PixelFloat, Heimdali::ImageDimension> VectorImageType;
+VectorImageType::Pointer vectorImage;
+
+// Command line tool readers.
+typedef Heimdali::CmdReader<VectorImageType> ReaderType;
+ReaderType* cmdreader = ReaderType::make_cmd_reader(0, input0.getValue());
+
+// Command line tool writer.
+typedef Heimdali::CmdWriter<VectorImageType> WriterType;
+WriterType* cmdwriter = WriterType::make_cmd_writer(output.getValue());
+
+// Duplicator.
+typedef itk::ImageDuplicator<ScalarImageType> DuplicatorType;
+DuplicatorType::Pointer duplicator = DuplicatorType::New();
+
+// VectorImage to Image
+typedef itk::VectorImageToImageAdaptor
+    <Heimdali::PixelFloat, Heimdali::ImageDimension> ToImageType;
+ToImageType::Pointer toImage = ToImageType::New();
+
+// Log filter.
+typedef itk::LogImageFilter <ToImageType,ScalarImageType> LogrType;
+LogrType::Pointer logfilter = LogrType::New();
+
+// Image to VectorImage
+typedef itk::ComposeImageFilter<ScalarImageType> ToVectorImageType;
+ToVectorImageType::Pointer toVectorImage = ToVectorImageType::New();
+
+size_t iregionmax = 1E+06;
+for (size_t iregion=0 ; iregion<iregionmax ; iregion++) {
+    // Read input.
+    cmdreader->next_iteration();
+    if (cmdreader->is_complete()) break;
+
+    cmdreader->reader()->Update();
+
+    vectorImage = cmdreader->GetOutput();
+
+    toImage->SetImage(vectorImage);
+
+    for (int ic = 0 ; ic < cmdreader->get_sc() ; ++ic)
+    {
+        // VectorImage to Image
+        toImage->SetExtractComponentIndex(ic);
+        toImage->Modified();
+
+        // Log images.
+        logfilter->SetInput(toImage);
+        logfilter->Update();
+        logfilter->Modified();
+
+        // Image to VectorImage
+        duplicator->SetInputImage(logfilter->GetOutput());
+        duplicator->Update();
+
+        // Image to VectorImage
+        toVectorImage->SetInput(ic, duplicator->GetOutput());
+        toVectorImage->Modified();
+        toVectorImage->Update();
+    }
+
+    // Write output.
+    cmdwriter->Write( toVectorImage->GetOutput() );
+    cmdwriter->Update();
+
+    vectorImage->Initialize();
+}
+
+} // End of 'try' block.
+
+
+// Command line parser.
+catch (TCLAP::ArgException &e) { 
+    cerr << "par: ERROR: " << e.error() << " for arg " << e.argId() << endl;
+}
+
+
+// Input/output.
+catch (Heimdali::IOError &e) {
+    cerr << "par: ERROR: " << e.getMessage() << endl;
+}
+
+catch (Heimdali::NotImplementedError &e) {
+    cerr << "par: ERROR: " << e.getMessage() << endl;
+}
+
+return 0;
+
+}
---
+++ tests/features/lo.feature
@@ -0,0 +1,9 @@
+Feature: The 'lo' command line tool.
+
+    Scenario: Invoke lo
+        When I run the command: lo --help
+        Then I see the line in standard output: Compute logarithm of each pixel
+
+    Scenario: Compute logarithm of each pixel
+        When I run the command: lo $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2.h5 -o lo_out.h5
+        Then the HDF5 files lo_out.h5 and $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2_lo.h5 are equal
