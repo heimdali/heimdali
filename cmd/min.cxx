5902c58d2a3b05e232618702de30395de2bdef22
David Froger <david.froger@inria.fr>
2015-02-12 16:18:57 +0100

    Add min command

---
+++ cmd/min.cxx
@@ -0,0 +1,155 @@
+#include <tclap/CmdLine.h>
+
+#include <itkImage.h>
+#include <itkMinimumImageFilter.h>
+#include <itkVectorImageToImageAdaptor.h>
+#include <itkComposeImageFilter.h>
+#include <itkImageDuplicator.h>
+#include <itkINRImageIOFactory.h>
+
+#include "heimdali/cmdreader.hxx"
+#include "heimdali/cmdwriter.hxx"
+#include "heimdali/itkhelper.hxx"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{ 
+
+try {
+
+TCLAP::CmdLine cmd("Minimum of two images", ' ', "0.0.0");
+
+// -o output.h5
+TCLAP::ValueArg<string> output("o","output", 
+    "Output image, instead of standard output",false,"","FILENAME", cmd);
+
+// -streaming N
+TCLAP::ValueArg<int> streaming("s","streaming", "Number of lines to stream",
+    false, 0,"NUMBER_OF_LINES", cmd);
+
+// --force
+TCLAP::SwitchArg forceSwitch("v","verbose",
+    "ITK HDF5 IO operation are verboses.", cmd);
+
+// image0.h5
+TCLAP::UnlabeledValueArg<string> input0("image0", 
+    "First image.",true,"","IMAGE1",cmd);
+
+// image1.h5
+TCLAP::UnlabeledValueArg<string> input1("image1", 
+    "Second image.",true,"","IMAGE2",cmd);
+
+cmd.parse(argc,argv);
+
+// Put our INRimage reader in the list of readers ITK knows.
+itk::ObjectFactoryBase::RegisterFactory( itk::INRImageIOFactory::New() ); 
+
+// Image type.
+typedef itk::Image<Heimdali::PixelFloat, Heimdali::ImageDimension> ScalarImageType;
+typedef itk::VectorImage<Heimdali::PixelFloat, Heimdali::ImageDimension> VectorImageType;
+VectorImageType::Pointer vectorImage1;
+VectorImageType::Pointer vectorImage2;
+
+// Command line tool readers.
+typedef Heimdali::CmdReader<VectorImageType> ReaderType;
+ReaderType* cmdreader1 = ReaderType::make_cmd_reader( streaming.getValue(),
+                                                      input0.getValue());
+
+ReaderType* cmdreader2 = ReaderType::make_cmd_reader(streaming.getValue(),
+                                                     input1.getValue());
+// Command line tool writer.
+typedef Heimdali::CmdWriter<VectorImageType> WriterType;
+WriterType* cmdwriter = WriterType::make_cmd_writer(output.getValue());
+
+// VectorImage to Image
+typedef itk::VectorImageToImageAdaptor
+    <Heimdali::PixelFloat, Heimdali::ImageDimension> ToImageType;
+ToImageType::Pointer toImage1 = ToImageType::New();
+ToImageType::Pointer toImage2 = ToImageType::New();
+
+// Minimum filter.
+typedef itk::MinimumImageFilter <ToImageType,ToImageType,ScalarImageType> MinimumImageFilterType;
+MinimumImageFilterType::Pointer minimizer = MinimumImageFilterType::New ();
+
+// Duplicator.
+typedef itk::ImageDuplicator<ScalarImageType> DuplicatorType;
+DuplicatorType::Pointer duplicator = DuplicatorType::New();
+
+// Image to VectorImage
+typedef itk::ComposeImageFilter<ScalarImageType> ToVectorImageType;
+ToVectorImageType::Pointer toVectorImage = ToVectorImageType::New();
+
+ScalarImageType::IndexType index;
+index[0] = 1;
+index[1] = 0;
+index[2] = 0;
+
+size_t iregionmax = 1E+06;
+for (size_t iregion=0 ; iregion<iregionmax ; iregion++) {
+    // Read input.
+    cmdreader1->next_iteration();
+    cmdreader2->next_iteration();
+    if (cmdreader1->is_complete()) break;
+
+    cmdreader1->reader()->Update();
+    cmdreader2->reader()->Update();
+
+    vectorImage1 = cmdreader1->GetOutput();
+    vectorImage2 = cmdreader2->GetOutput();
+
+    toImage1->SetImage(vectorImage1);
+    toImage2->SetImage(vectorImage2);
+
+    for (int ic = 0 ; ic < cmdreader1->get_sc() ; ++ic)
+    {
+        // VectorImage to Image
+        toImage1->SetExtractComponentIndex(ic);
+        toImage2->SetExtractComponentIndex(ic);
+        toImage1->Modified();
+        toImage2->Modified();
+
+        // Minimum images.
+        minimizer->SetInput1(toImage1);
+        minimizer->SetInput2(toImage2);
+        minimizer->Update();
+        minimizer->Modified();
+
+        // Image to VectorImage
+        duplicator->SetInputImage(minimizer->GetOutput());
+        duplicator->Update();
+
+        toVectorImage->SetInput(ic, duplicator->GetOutput());
+        toVectorImage->Modified();
+        toVectorImage->Update();
+    }
+
+    // Write output.
+    cmdwriter->Write( toVectorImage->GetOutput() );
+    cmdwriter->Update();
+
+    vectorImage1->Initialize();
+    vectorImage2->Initialize();
+}
+
+} // End of 'try' block.
+
+
+// Command line parser.
+catch (TCLAP::ArgException &e) { 
+    cerr << "par: ERROR: " << e.error() << " for arg " << e.argId() << endl;
+}
+
+
+// Input/output.
+catch (Heimdali::IOError &e) {
+    cerr << "par: ERROR: " << e.getMessage() << endl;
+}
+
+catch (Heimdali::NotImplementedError &e) {
+    cerr << "par: ERROR: " << e.getMessage() << endl;
+}
+
+return 0;
+
+}
---
+++ tests/features/min.feature
@@ -0,0 +1,9 @@
+Feature: The 'min' command line tool.
+
+    Scenario: Invoke min
+        When I run the command: min --help
+        Then I see the line in standard output: Minimum of two images
+
+    Scenario: Minimum of two images
+        When I run the command: min -o min_out.h5 $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2.h5 $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2_bi-1000_pow2.h5
+        Then the HDF5 files min_out.h5 and $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2_bi-1000_pow2_min_with_orig.h5 are equal
