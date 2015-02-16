65ab3f1a6b2326befe68ab7e1a88c4cd0a612063
David Froger <david.froger@inria.fr>
2015-02-16 14:14:04 +0100

    Limit number of make's jobs to 2 on travis

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -5,10 +5,19 @@ export LIBRARY_PATH=""
 mkdir build
 cd build
 
+if [ "$TRAVIS" == "true" ]
+then
+    MAKE_JOBS=2
+else
+    MAKE_JOBS=$CPU_COUNT
+fi
+
+echo $MAKE_JOBS
+
 # CMake detect ITK_DIR as $PREFIX/lib/cmake/ITK-4.7
 cmake \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_PREFIX_PATH=$PREFIX \
   ..
-make -j$CPU_COUNT
+make -j$MAKE_JOBS
 make install
