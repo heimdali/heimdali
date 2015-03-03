9d8c3bf6a05f7cb51a436a961d6a4694c4e0db93
David Froger <david.froger@inria.fr>
2015-03-03 15:27:56 +0100

    ra can read/write from/to stdin/stdout
    This fix a part of #14

--- cmd/ra.cxx
+++ cmd/ra.cxx
@@ -11,6 +11,7 @@
 #include "heimdali/cmdwriter.hxx"
 #include "heimdali/itkhelper.hxx"
 #include "heimdali/version.hxx"
+#include "heimdali/cli.hxx"
 
 using namespace std;
 
@@ -19,17 +20,13 @@ int main(int argc, char** argv)
 
 try {
 
-TCLAP::CmdLine cmd("Compute square root of each pixel", ' ', HEIMDALI_VERSION);
+TCLAP::CmdLine parser("Compute square root of each pixel", ' ', HEIMDALI_VERSION);
+HEIMDALI_TCLAP_IMAGE_IN_IMAGE_OUT(filenamesArg,parser)
 
-// -o output.h5
-TCLAP::ValueArg<string> output("o","image_out", 
-    "Output image, instead of standard output",false,"","FILENAME", cmd);
-
-// input.h5
-TCLAP::UnlabeledValueArg<string> input0("image_in", 
-    "First image.",true,"","IMAGE-IN",cmd);
-
-cmd.parse(argc,argv);
+parser.parse(argc,argv);
+string inputFilename;
+string outputFilename;
+Heimdali::parse_tclap_image_in_image_out(filenamesArg, inputFilename, outputFilename);
 
 // Put our INRimage reader in the list of readers ITK knows.
 itk::ObjectFactoryBase::RegisterFactory( itk::INRImageIOFactory::New() ); 
@@ -41,11 +38,11 @@ VectorImageType::Pointer vectorImage;
 
 // Command line tool readers.
 typedef Heimdali::CmdReader<VectorImageType> ReaderType;
-ReaderType* cmdreader = ReaderType::make_cmd_reader(0, input0.getValue());
+ReaderType* cmdreader = ReaderType::make_cmd_reader(0, inputFilename);
 
 // Command line tool writer.
 typedef Heimdali::CmdWriter<VectorImageType> WriterType;
-WriterType* cmdwriter = WriterType::make_cmd_writer(output.getValue());
+WriterType* cmdwriter = WriterType::make_cmd_writer(outputFilename);
 
 // Duplicator.
 typedef itk::ImageDuplicator<ScalarImageType> DuplicatorType;
--- tests/features/ra.feature
+++ tests/features/ra.feature
@@ -5,5 +5,13 @@ Feature: ra
         Then I see the line in standard output: Compute square root of each pixel
 
     Scenario: Compute square root of each pixel
-        When I run the command: ra $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2.h5 -o ra_out.h5
-        Then images ra_out.h5 and $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2_ra.h5 are equal
+        When I run the command: ra $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2.<ext> imtest_z5_y4_x3_c2_ra.<ext>
+        Then images imtest_z5_y4_x3_c2_ra.<ext> and $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2_ra.<ext> are equal
+    Examples:
+      | ext |
+      | h5  |
+      | inr |
+
+    Scenario: Read from standard input and write to standard output
+        When I run the command: cat $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2.h5 | ra > imtest_z5_y4_x3_c2_ra_stdin.h5
+        Then images imtest_z5_y4_x3_c2_ra_stdin.h5 and $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2_ra.h5 are equal
