51d5a668b720dc2caa42fd42a301d2897e20dd6a
David Froger <david.froger@inria.fr>
2015-03-04 22:34:53 +0100

    tpr: print image region (float) as expected

--- cmd/tpr.cxx
+++ cmd/tpr.cxx
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <algorithm>
 
 #include <tclap/CmdLine.h>
 
@@ -34,14 +35,14 @@ int main(int argc, char** argv)
     TCLAP::CmdLine cmd("Print the pixel values of a image subregion",' ', HEIMDALI_VERSION);
 
     // -iz -iy -ix
-    TCLAP::ValueArg<int> ixValue("","iz", "First column",false,0,"IZ",cmd);
-    TCLAP::ValueArg<int> iyValue("","iy", "First rows",false,0,"IY",cmd);
-    TCLAP::ValueArg<int> izValue("","ix", "First plane",false,0,"IX",cmd);
+    TCLAP::ValueArg<unsigned int> izArg("","iz", "First plane",false,0,"IZ",cmd);
+    TCLAP::ValueArg<unsigned int> iyArg("","iy", "First row",false,0,"IY",cmd);
+    TCLAP::ValueArg<unsigned int> ivArg("","ix", "First value",false,0,"IX",cmd);
 
     // -z -y -x
-    TCLAP::ValueArg<int> zValue("z","nplanes", "Number of planes",false,0,"NZ", cmd);
-    TCLAP::ValueArg<int> yValue("y","nrows", "Number of rows",false,0,"NY", cmd);
-    TCLAP::ValueArg<int> xValue("x","ncolumns", "Number of columns",false,0,"NX", cmd);
+    TCLAP::ValueArg<unsigned int> zArg("z","nplanes", "Number of planes",false,0,"NZ", cmd);
+    TCLAP::ValueArg<unsigned int> yArg("y","nrows", "Number of rows",false,0,"NY", cmd);
+    TCLAP::ValueArg<unsigned int> vArg("x","ncolumns", "Number of values",false,0,"NX", cmd);
 
     // input.h5
     TCLAP::UnlabeledMultiArg<string> inputFilenamesArg("inputFilenames", 
@@ -62,7 +63,7 @@ int main(int argc, char** argv)
 
     // Images
     const unsigned int Dimension = 3;
-    unsigned int SZ,SY,SX,SV, NZ,NY,NX, IZ,IY,IX;
+    unsigned int SZ,SY,SX,SC, NZ,NY,NX,NV, IZ,IY,IX,IV;
     typedef itk::VectorImage<PixelType, Dimension> ImageType;
     ImageType::Pointer image;
 
@@ -90,17 +91,41 @@ int main(int argc, char** argv)
     SZ = reader->GetImageIO()->GetDimensions(2);
     SY = reader->GetImageIO()->GetDimensions(1);
     SX = reader->GetImageIO()->GetDimensions(0);
-    SV = reader->GetImageIO()->GetNumberOfComponents();
+    SC = reader->GetImageIO()->GetNumberOfComponents();
 
-    // First index to read.
-    IZ = izValue.getValue();
-    IY = iyValue.getValue();
-    IX = ixValue.getValue();
+    // First plane to read and print
+    IZ = izArg.getValue();
 
-    // Number of plane/column/row read.
-    NZ = zValue.getValue()==0 ? SZ-IZ : zValue.getValue();
-    NY = yValue.getValue()==0 ? SY-IY : yValue.getValue();
-    NX = xValue.getValue()==0 ? SX-IX : xValue.getValue();
+    // First row to read and print
+    IY = iyArg.getValue();
+
+    // First value in row to print
+    IV = ivArg.getValue();
+
+    // The row will be read.
+    IX = 0;
+    NX = SX;
+
+    // Number of planes to print.
+    if (zArg.getValue() == 0) {
+        NZ = SZ-IZ;
+    } else {
+        NZ = std::min(zArg.getValue(), SZ-IZ);
+    }
+
+    // Number of rows to print.
+    if (yArg.getValue() == 0) {
+        NY = SY-IY;
+    } else {
+        NY = std::min(yArg.getValue(), SY-IY);
+    }
+
+    // Number of values to print per row.
+    if (vArg.getValue() == 0) {
+        NV = SX*SC - IV;
+    } else {
+        NV = std::min(vArg.getValue(), SX*SC - IV);
+    }
 
     // Create region to read
     ImageIndex[2] = IZ;
@@ -123,25 +148,30 @@ int main(int argc, char** argv)
     // Print values.
     typedef itk::VariableLengthVector<PixelType> VariableVectorType;
     VariableVectorType value;
-    value.SetSize(SV);
-    int nprinted = 0;
-    int nprinted_by_console_line = 5;
+    value.SetSize(SC);
+    unsigned int nprinted = 0;
+    unsigned int nprinted_by_console_line = 5;
+    unsigned int iv;
     for (unsigned int iz=IZ ; iz<IZ+NZ; iz++) { 
         ImageIndex[2] = iz;
         for (unsigned int iy=IY ; iy<IY+NY; iy++) { 
             cout << "plane " << iz << ", line " << iy << endl;
             ImageIndex[1] = iy;
+            iv = -1;
             for (unsigned int ix=IX ; ix<IX+NX; ix++) {
                 ImageIndex[0] = ix;
                 value = image->GetPixel(ImageIndex);
-                for (unsigned int iv=0 ; iv<SV ; iv++) {
+                for (unsigned int ic=0 ; ic<SC ; ic++) {
+                    iv++;
+                    if (! (IV<=iv && iv<IV+NV) )
+                        continue;
                     nprinted++;
                     if (nprinted == nprinted_by_console_line || 
-                            (iv==SV-1 && ix==IX+NX-1) ) {
-                        cout << value[iv] << endl;
+                            (iv==IV+NV-1) ) {
+                        cout << value[ic] << endl;
                         nprinted = 0;
                     } else {
-                        cout << value[iv] << " ";
+                        cout << value[ic] << " ";
                     }
                 }
             }
--- tests/features/tpr.feature
+++ tests/features/tpr.feature
@@ -14,12 +14,12 @@ Feature: tpr
 #       | cossin_z5_y5_x5_c2_f2.h5  | tpr_cossin_f2.txt |
 #       | cossin_z5_y5_x5_c2_f2.inr | tpr_cossin_f2.txt |
 
-#   Scenario: Print a image region
-#       When I run the command: tpr -ix 1 -iy 1 -iz 1 -x 2 -y 2 -z 2  $HEIMDALI_DATA_DIR/<input>
-#       Then I see as standard output the content of the file $HEIMDALI_DATA_DIR/<stdout>
-#   Examples:
-#       | input                     | stdout                   |
-#       | cossin_z5_y5_x5_c2_r4.h5  | tpr_cossin_r4_region.txt |
-#       | cossin_z5_y5_x5_c2_r4.inr | tpr_cossin_r4_region.txt |
+    Scenario: Print a image region
+       When I run the command: tpr -ix 1 -iy 1 -iz 1 -x 2 -y 2 -z 2  $HEIMDALI_DATA_DIR/<input>
+       Then I see as standard output the content of the file $HEIMDALI_DATA_DIR/<stdout>
+   Examples:
+       | input                     | stdout                   |
+        | cossin_z5_y5_x5_c2_r4.h5  | tpr_cossin_r4_region.txt |
+        | cossin_z5_y5_x5_c2_r4.inr | tpr_cossin_r4_region.txt |
 #       | cossin_z5_y5_x5_c2_f2.h5  | tpr_cossin_f2_region.txt |
 #       | cossin_z5_y5_x5_c2_f2.inr | tpr_cossin_f2_region.txt |
