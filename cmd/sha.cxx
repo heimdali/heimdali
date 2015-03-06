9fdf14839568b86408473cd30f30dcfa63a8349a
David Froger <david.froger@inria.fr>
2015-03-06 15:33:41 +0100

    Add sha command

---
+++ cmd/sha.cxx
@@ -0,0 +1,119 @@
+#include <tclap/CmdLine.h>
+
+#include <itkImage.h>
+#include <itkComposeImageFilter.h>
+#include <itkImageDuplicator.h>
+#include <itkThresholdImageFilter.h>
+#include <itkINRImageIOFactory.h>
+#include <itkVectorIndexSelectionCastImageFilter.h>
+
+#include "heimdali/cmdreader.hxx"
+#include "heimdali/cmdwriter.hxx"
+#include "heimdali/itkhelper.hxx"
+#include "heimdali/version.hxx"
+#include "heimdali/cli.hxx"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{ 
+
+try {
+
+TCLAP::CmdLine parser("Set image values to THRESH if values are above THRESH",
+                      ' ', HEIMDALI_VERSION);
+// -n
+TCLAP::ValueArg<float> numberArg("n","number", "Threshold value",true,
+                                 1,"NUNMBER", parser);
+
+HEIMDALI_TCLAP_IMAGE_IN_IMAGE_OUT(filenamesArg,parser)
+
+parser.parse(argc,argv);
+string inputFilename;
+string outputFilename;
+Heimdali::parse_tclap_image_in_image_out(filenamesArg, inputFilename, outputFilename);
+
+// Put our INRimage reader in the list of readers ITK knows.
+itk::ObjectFactoryBase::RegisterFactory( itk::INRImageIOFactory::New() ); 
+
+// Image type.
+typedef itk::Image<Heimdali::PixelFloat, Heimdali::ImageDimension> ScalarImageType;
+typedef itk::VectorImage<Heimdali::PixelFloat, Heimdali::ImageDimension> VectorImageType;
+
+// Command line tool readers.
+typedef Heimdali::CmdReader<VectorImageType> ReaderType;
+ReaderType* cmdreader = ReaderType::make_cmd_reader(0, inputFilename);
+
+// Command line tool writer.
+typedef Heimdali::CmdWriter<VectorImageType> WriterType;
+WriterType* cmdwriter = WriterType::make_cmd_writer(outputFilename);
+
+// indexer
+typedef itk::VectorIndexSelectionCastImageFilter<VectorImageType, ScalarImageType> IndexerType;
+IndexerType::Pointer indexer = IndexerType::New();
+
+// thresholder
+typedef itk::ThresholdImageFilter<ScalarImageType> ThresholderType;
+typename ThresholderType::Pointer thresholder = ThresholderType::New();
+thresholder->ThresholdAbove(numberArg.getValue());
+thresholder->SetOutsideValue(numberArg.getValue());
+
+// duplicator
+typedef itk::ImageDuplicator<ScalarImageType> DuplicatorType;
+DuplicatorType::Pointer duplicator = DuplicatorType::New();
+
+// composer
+typedef itk::ComposeImageFilter<ScalarImageType> ComposerType;
+ComposerType::Pointer composer = ComposerType::New();
+
+unsigned int iregionmax = 1E+06;
+for (unsigned int iregion=0 ; iregion<iregionmax ; iregion++) {
+    // Read input.
+    cmdreader->next_iteration();
+    if (cmdreader->is_complete()) break;
+    cmdreader->reader()->Update();
+
+    indexer->SetInput(cmdreader->GetOutput());
+
+    for (unsigned int componentIndex = 0 ;
+                      componentIndex < cmdreader->get_sc() ;
+                      componentIndex++)
+    {
+        indexer->SetIndex(componentIndex);
+
+        thresholder->SetInput(indexer->GetOutput());
+        thresholder->Update();
+
+        duplicator->SetInputImage(thresholder->GetOutput());
+        duplicator->Update();
+
+        composer->SetInput(componentIndex, duplicator->GetOutput());
+    }
+    composer->Update();
+
+    // Write output.
+    cmdwriter->Write( composer->GetOutput() );
+    cmdwriter->Update();
+}
+
+} // End of 'try' block.
+
+
+// Command line parser.
+catch (TCLAP::ArgException &e) { 
+    cerr << "sha: ERROR: " << e.error() << " for arg " << e.argId() << endl;
+}
+
+
+// Input/output.
+catch (Heimdali::IOError &e) {
+    cerr << "sha: ERROR: " << e.getMessage() << endl;
+}
+
+catch (Heimdali::NotImplementedError &e) {
+    cerr << "sha: ERROR: " << e.getMessage() << endl;
+}
+
+return 0;
+
+}
---
+++ tests/features/sha.feature
@@ -0,0 +1,17 @@
+Feature: sha
+
+    Scenario: Invoke sha
+        When I run the command: sha --help
+        Then I see the line in standard output: Set image values to THRESH if values are above THRESH
+
+    Scenario: Threshold an image
+        When I run the command: sha -n 0.5 $HEIMDALI_DATA_DIR/cossin_z5_y5_x5_c2_r4.<ext> cossin_z5_y5_x5_c2_r4_sha_0_5.<ext>
+        Then images cossin_z5_y5_x5_c2_r4_sha_0_5.<ext> and $HEIMDALI_DATA_DIR/cossin_z5_y5_x5_c2_r4_sha_0_5.<ext> are equal
+    Examples:
+      | ext |
+      | h5  |
+      | inr |
+
+    Scenario: Read from standard input and write to standard output
+        When I run the command: cat $HEIMDALI_DATA_DIR/cossin_z5_y5_x5_c2_r4.h5 | sha -n 0.5 > cossin_z5_y5_x5_c2_r4_sha_0_5_sdtin.h5
+        Then images cossin_z5_y5_x5_c2_r4_sha_0_5_sdtin.h5 and $HEIMDALI_DATA_DIR/cossin_z5_y5_x5_c2_r4_sha_0_5.h5 are equal
