26990d84542adc01421ce01f3bb3fe6ab89c82f7
David Froger <david.froger@inria.fr>
2015-03-07 17:05:16 +0100

    Add logic CLI

--- cmd/logic.cxx
+++ cmd/logic.cxx
@@ -14,7 +14,18 @@ try {
 TCLAP::CmdLine parser("Boolean operation on image",
                       ' ', HEIMDALI_VERSION);
 
-parser.parse(argc,argv);
+HEIMDALI_TCLAP_IMAGE_IN_IMAGE_OUT(filenamesArg,parser)
+
+TCLAP::SwitchArg andSwitch("","et", "Boolean and", parser, false);
+TCLAP::SwitchArg orSwitch("","ou", "Boolean or", parser, false);
+TCLAP::SwitchArg xorSwitch("","xou", "Boolean xor", parser, false);
+TCLAP::ValueArg<int> maArg("","ma","And with hexadecimal constant HH",false,0,"HH",parser);
+TCLAP::ValueArg<int> foArg("","fo","Or with hexadecimal constant HH",false,0,"HH",parser);
+TCLAP::ValueArg<int> coArg("","co","Xor with hexadecimal constant HH",false,0,"HH",parser);
+TCLAP::SwitchArg invSwitch("","inv", "", parser, false);
+
+vector<string> tclap_argv = Heimdali::preprocess_argv(argc, argv);
+parser.parse(tclap_argv);
 
 } // End of 'try' block.
 
