e3c2ddcdffd99a504513fbc219c59af8d315d32c
David Froger <david.froger@inria.fr>
2015-03-09 16:57:05 +0100

    Implement logic -et

--- cmd/logic.cxx
+++ cmd/logic.cxx
@@ -1,5 +1,14 @@
 #include <tclap/CmdLine.h>
 
+#include "itkVectorImage.h"
+#include "itkAndImageFilter.h"
+#include <itkINRImageIOFactory.h>
+#include <itkComposeImageFilter.h>
+#include <itkImageDuplicator.h>
+#include <itkVectorIndexSelectionCastImageFilter.h>
+
+#include "heimdali/cmdreader.hxx"
+#include "heimdali/cmdwriter.hxx"
 #include "heimdali/version.hxx"
 #include "heimdali/cli.hxx"
 #include "heimdali/error.hxx"
@@ -26,6 +35,80 @@ TCLAP::SwitchArg invSwitch("","inv", "", parser, false);
 
 vector<string> tclap_argv = Heimdali::preprocess_argv(argc, argv);
 parser.parse(tclap_argv);
+string inputFilename0;
+string inputFilename1;
+string outputFilename;
+Heimdali::parse_tclap_image_in_image_in_image_out(filenamesArg, inputFilename0,
+                                                  inputFilename1, outputFilename);
+
+// Put our INRimage reader in the list of readers ITK knows.
+itk::ObjectFactoryBase::RegisterFactory( itk::INRImageIOFactory::New() ); 
+
+// Image type.
+typedef unsigned char PixelType;
+const unsigned int Dimension = 3;
+typedef itk::Image<PixelType, Dimension> ScalarImageType;
+typedef itk::VectorImage<PixelType, Dimension> VectorImageType;
+
+// Command line tool readers.
+typedef Heimdali::CmdReader<VectorImageType> ReaderType;
+ReaderType* reader0 = ReaderType::make_cmd_reader(0, inputFilename0);
+ReaderType* reader1 = ReaderType::make_cmd_reader(0, inputFilename1);
+
+// indexer
+typedef itk::VectorIndexSelectionCastImageFilter<VectorImageType, ScalarImageType> IndexerType;
+IndexerType::Pointer indexer0 = IndexerType::New();
+IndexerType::Pointer indexer1 = IndexerType::New();
+
+// duplicator
+typedef itk::ImageDuplicator<ScalarImageType> DuplicatorType;
+DuplicatorType::Pointer duplicator = DuplicatorType::New();
+
+// composer
+typedef itk::ComposeImageFilter<ScalarImageType> ComposerType;
+ComposerType::Pointer composer = ComposerType::New();
+
+// AndFillter
+typedef itk::AndImageFilter<ScalarImageType> AndFilterType;
+AndFilterType::Pointer andFilter = AndFilterType::New();
+
+// Command line tool writer.
+typedef Heimdali::CmdWriter<VectorImageType> WriterType;
+WriterType* cmdwriter = WriterType::make_cmd_writer(outputFilename);
+
+unsigned int iregionmax = 1E+06;
+for (unsigned int iregion=0 ; iregion<iregionmax ; iregion++) {
+    // Read input.
+    reader0->next_iteration();
+    reader1->next_iteration();
+    if (reader0->is_complete()) break;
+
+    indexer0->SetInput(reader0->GetOutput());
+    indexer1->SetInput(reader1->GetOutput());
+
+    for (unsigned int componentIndex = 0 ;
+                      componentIndex < reader0->get_sc() ;
+                      componentIndex++)
+    {
+        indexer0->SetIndex(componentIndex);
+        indexer1->SetIndex(componentIndex);
+
+        // Binary AND images.
+        andFilter->SetInput(0, indexer0->GetOutput());
+        andFilter->SetInput(1, indexer1->GetOutput());
+        andFilter->Update();
+
+        duplicator->SetInputImage(andFilter->GetOutput());
+        duplicator->Update();
+
+        composer->SetInput(componentIndex, duplicator->GetOutput());
+    }
+    composer->Update();
+
+    // Write output.
+    cmdwriter->Write( composer->GetOutput() );
+    cmdwriter->Update();
+}
 
 } // End of 'try' block.
 
--- tests/features/logic.feature
+++ tests/features/logic.feature
@@ -3,3 +3,12 @@ Feature: logic
     Scenario: Invoke logic
         When I run the command: logic --help
         Then I see the line in standard output: Boolean operation on image
+
+    Scenario: Binary AND two images
+        When I run the command: logic -et $HEIMDALI_DATA_DIR/boolean_f1.<ext> $HEIMDALI_DATA_DIR/boolean_another_f1.<ext> boolean_and_f1.<ext>
+        Then images boolean_and_f1.<ext> and $HEIMDALI_DATA_DIR/boolean_and_f1.<ext> are equal
+
+    Examples:
+    | ext |
+    | h5  |
+    | inr |
