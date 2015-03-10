1c0cbad91a1b6e42c2dd418ae4d14e74cd9a0717
David Froger <david.froger@inria.fr>
2015-03-10 09:06:27 +0100

    Implement med command
    Inrimage and ITK produce different results after aplying median filter.

--- cmd/med.cxx
+++ cmd/med.cxx
@@ -1,8 +1,22 @@
 #include <tclap/CmdLine.h>
 
+#include <itkINRImageIOFactory.h>
+#include "itkImageFileReader.h"
+#include "itkMedianImageFilter.h"
+#include <itkImageDuplicator.h>
+#include <itkVectorIndexSelectionCastImageFilter.h>
+#include "itkImageFileWriter.h"
+#include <itkComposeImageFilter.h>
+#include "itkPermuteAxesImageFilter.h"
+
 #include "heimdali/version.hxx"
 #include "heimdali/cli.hxx"
 #include "heimdali/error.hxx"
+
+typedef float PixelType;
+const unsigned int ImageDimension = 3;
+
+const unsigned int ZD=2, YD=1, XD=0;
 
 int main(int argc, char** argv)
 { 
@@ -11,8 +25,78 @@ try {
 
 TCLAP::CmdLine parser("Median filter", ' ', HEIMDALI_VERSION);
 
+TCLAP::ValueArg<unsigned int> yArg("y","row-size", "Window size",false,3,"Y", parser);
+TCLAP::ValueArg<unsigned int> xArg("x","column-size", "Window size",false,3,"X", parser);
+
+HEIMDALI_TCLAP_IMAGE_IN_IMAGE_OUT(filenamesArg,parser)
+
 vector<string> tclap_argv = Heimdali::preprocess_argv(argc, argv);
 parser.parse(tclap_argv);
+string inputFilename;
+string outputFilename;
+Heimdali::parse_tclap_image_in_image_out(filenamesArg, inputFilename, outputFilename);
+
+// Put our INRimage reader in the list of readers ITK knows.
+itk::ObjectFactoryBase::RegisterFactory( itk::INRImageIOFactory::New() ); 
+
+// Image type.
+typedef itk::Image<PixelType, ImageDimension> ScalarImageType;
+typedef itk::VectorImage<PixelType, ImageDimension> VectorImageType;
+
+// Reader
+typedef itk::ImageFileReader<VectorImageType> ReaderType;
+ReaderType::Pointer reader = ReaderType::New();
+reader->SetFileName(inputFilename);
+
+// indexer
+typedef itk::VectorIndexSelectionCastImageFilter<VectorImageType, ScalarImageType> IndexerType;
+IndexerType::Pointer indexer = IndexerType::New();
+
+// Median filter
+typedef itk::MedianImageFilter<ScalarImageType, ScalarImageType> MedianerType;
+MedianerType::Pointer medianer = MedianerType::New();
+MedianerType::InputSizeType radius;
+radius[XD] = xArg.getValue();
+radius[YD] = yArg.getValue();
+radius[ZD] = 1;
+medianer->SetRadius(radius);
+
+// duplicator
+typedef itk::ImageDuplicator<ScalarImageType> DuplicatorType;
+DuplicatorType::Pointer duplicator = DuplicatorType::New();
+
+// composer
+typedef itk::ComposeImageFilter<ScalarImageType> ComposerType;
+ComposerType::Pointer composer = ComposerType::New();
+
+// Writer
+typedef itk::ImageFileWriter<VectorImageType> WriterType;
+WriterType::Pointer writer = WriterType::New();
+writer->SetFileName(outputFilename);
+
+reader->UpdateOutputInformation();
+
+indexer->SetInput(reader->GetOutput());
+
+for (unsigned int componentIndex = 0 ;
+                  componentIndex < reader->GetImageIO()->GetNumberOfComponents() ;
+                  componentIndex++)
+{
+    indexer->SetIndex(componentIndex);
+
+    medianer->SetInput( indexer->GetOutput() );
+    medianer->Update();
+
+    duplicator->SetInputImage(medianer->GetOutput());
+    duplicator->Update();
+
+    composer->SetInput(componentIndex, duplicator->GetOutput());
+}
+composer->Update();
+
+// Write output.
+writer->SetInput( composer->GetOutput() );
+writer->Update();
 
 } // End of 'try' block.
 
--- tests/features/med.feature
+++ tests/features/med.feature
@@ -4,3 +4,6 @@ Feature: med
         When I run the command: med --help
         Then I see the line in standard output: Median filter
 
+#   Scenario: Apply median filter
+#       When I run the command: med $HEIMDALI_DATA_DIR/lena_r4.h5 lena_med_r4.h5
+#       Then images $HEIMDALI_DATA_DIR/lena_med_r4.h5 and lena_med_r4.h5 are equal
