4690fb52b29ef016c0f4be5ce6a150adc7722f4e
David Froger <david.froger@inria.fr>
2015-03-13 14:57:57 +0100

    ddf, wip

---
+++ cmd/ddf.cxx
@@ -0,0 +1,139 @@
+#include <tclap/CmdLine.h>
+
+#include <itkImage.h>
+#include <itkINRImageIOFactory.h>
+#include <itkComplexToComplexFFTImageFilter.h>
+#include <itkVnlComplexToComplexFFTImageFilter.h>
+#include "itkComplexToRealImageFilter.h"
+#include "itkComplexToImaginaryImageFilter.h"
+#include <itkVectorIndexSelectionCastImageFilter.h>
+#include <itkImageDuplicator.h>
+#include <itkComposeImageFilter.h>
+
+#include "heimdali/cmdreader.hxx"
+#include "heimdali/cmdwriter.hxx"
+#include "heimdali/version.hxx"
+#include "heimdali/cli.hxx"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{ 
+
+TCLAP::CmdLine parser("Direct FFT on complex image", ' ', HEIMDALI_VERSION);
+
+HEIMDALI_TCLAP_IMAGE_IN_IMAGE_IN_IMAGE_OUT_IMAGE_OUT(filenamesArg,parser)
+
+parser.parse(argc,argv);
+string inputFilename_re;
+string inputFilename_im;
+string outputFilename_re;
+string outputFilename_im;
+Heimdali::parse_tclap_image_in_image_in_image_out_image_out(filenamesArg,
+                                                   inputFilename_re, inputFilename_im,
+                                                   outputFilename_re, outputFilename_im);
+
+// Put our INRimage reader in the list of readers ITK knows.
+itk::ObjectFactoryBase::RegisterFactory( itk::INRImageIOFactory::New() ); 
+
+// Image type.
+typedef float PixelType;
+const unsigned int Dimension = 2;
+typedef itk::Image<PixelType, Dimension> ScalarImageType;
+typedef itk::Image<std::complex<float>, Dimension> ComplexScalarImageType;
+
+// Readers
+typedef itk::ImageFileReader<ScalarImageType> ReaderType;
+ReaderType::Pointer reader_re = ReaderType::New();
+ReaderType::Pointer reader_im = ReaderType::New();
+reader_re->SetFileName(inputFilename_re);
+reader_im->SetFileName(inputFilename_im);
+reader_re->Update();
+reader_im->Update();
+
+// complexer
+typedef itk::ComposeImageFilter<ScalarImageType,ComplexScalarImageType> ComplexerType;
+ComplexerType::Pointer complexer = ComplexerType::New();
+complexer->SetInput1(reader_re->GetOutput());
+complexer->SetInput2(reader_im->GetOutput());
+complexer->Update();
+         
+
+// FFT
+typedef itk::VnlComplexToComplexFFTImageFilter<ComplexScalarImageType> FFTType;
+FFTType::Pointer fftFilter = FFTType::New();
+
+/*
+// Extract the real part
+typedef itk::ComplexToRealImageFilter< FFTOutputImageType, ScalarImageType> RealFilterType;
+RealFilterType::Pointer refilter = RealFilterType::New();
+
+// Extract the imaginary part
+typedef itk::ComplexToImaginaryImageFilter< FFTOutputImageType, ScalarImageType> ImaginaryFilterType;
+ImaginaryFilterType::Pointer imfilter = ImaginaryFilterType::New();
+
+// duplicator
+typedef itk::ImageDuplicator<ScalarImageType> DuplicatorType;
+DuplicatorType::Pointer duplicator_re = DuplicatorType::New();
+DuplicatorType::Pointer duplicator_im = DuplicatorType::New();
+
+// composer
+typedef itk::ComposeImageFilter<ScalarImageType> ComposerType;
+ComposerType::Pointer composer_re = ComposerType::New();
+ComposerType::Pointer composer_im = ComposerType::New();
+
+// Command line tool writer.
+typedef Heimdali::CmdWriter<VectorImageType> WriterType;
+WriterType* writer_re = WriterType::make_cmd_writer(outputFilename_re);
+WriterType* writer_im = WriterType::make_cmd_writer(outputFilename_im);
+
+
+unsigned int iregionmax = 1E+06;
+for (unsigned int iregion=0 ; iregion<iregionmax ; iregion++) {
+    // Read input.
+    reader->next_iteration();
+    if (reader->is_complete()) break;
+
+    indexer->SetInput(reader->GetOutput());
+
+    for (unsigned int componentIndex = 0 ;
+                      componentIndex < reader->get_sc();
+                      componentIndex++)
+    {
+        indexer->SetIndex(componentIndex);
+
+        // FFT
+        fftFilter->SetInput( indexer->GetOutput() );
+
+        // Extract real and imaginary part.
+        refilter->SetInput(fftFilter->GetOutput());
+        imfilter->SetInput(fftFilter->GetOutput());
+        refilter->Update();
+        imfilter->Update();
+        refilter->Modified();
+        imfilter->Modified();
+
+        duplicator_re->SetInputImage(refilter->GetOutput());
+        duplicator_im->SetInputImage(imfilter->GetOutput());
+        duplicator_re->Update();
+        duplicator_im->Update();
+
+        composer_re->SetInput(componentIndex, duplicator_re->GetOutput());
+        composer_im->SetInput(componentIndex, duplicator_im->GetOutput());
+        composer_re->Modified();
+        composer_im->Modified();
+        composer_re->Update();
+        composer_im->Update();
+    }
+
+    // Write output.
+    writer_re->Write( composer_re->GetOutput() );
+    writer_im->Write( composer_im->GetOutput() );
+    writer_re->Update();
+    writer_im->Update();
+}
+*/
+
+return 0;
+
+}
---
+++ tests/features/ddf.feature
@@ -0,0 +1,8 @@
+Feature: ddf
+
+    Scenario: Invoke ddf
+        When I run the command: ddf --help
+        Then I see the line in standard output: Direct FFT on complex image
+
+    Scenario: Compute direct FFT on complex image
+        When I run the command: ddf $HEIMDALI_DATA_DIR/lena_r4_rdf_re.h5 $HEIMDALI_DATA_DIR/lena_r4_rdf_im.h5 lena_r4_rdf_re.h5 lena_r4_rdf_im.h5
