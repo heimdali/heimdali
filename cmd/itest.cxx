67f88f072fa3a12ea2cef456ad2cc2406c6a27a9
David Froger <david.froger@inria.fr>
2015-03-16 16:15:02 +0100

    test: add -x0 -y0 -z0

--- cmd/itest.cxx
+++ cmd/itest.cxx
@@ -17,7 +17,7 @@ template <typename S, typename T>
 void
 test_value(string label, S value, T expected_value, ostringstream& error_msg)
 {
-    if (expected_value != 0 && value != expected_value)
+    if (value != expected_value)
         error_msg << label << " are different: " << value 
                   << " VS " << expected_value << " ." << endl;;
 }
@@ -37,6 +37,11 @@ int main(int argc, char** argv)
     TCLAP::ValueArg<unsigned int> yValue("y","nrows", "Number of rows",false,0,"NY", parser);
     TCLAP::ValueArg<unsigned int> xValue("x","ncolumns", "Number of columns",false,0,"NX", parser);
     TCLAP::ValueArg<unsigned int> vValue("v","component", "Number of pixel components",false,0, "NV", parser);
+
+    // -z0 -y0 -x0
+    TCLAP::ValueArg<unsigned int> z0Value("","z0", "z origin",false,0,"Z0", parser);
+    TCLAP::ValueArg<unsigned int> y0Value("","y0", "y origin",false,0,"Y0", parser);
+    TCLAP::ValueArg<unsigned int> x0Value("","x0", "x origin",false,0,"X0", parser);
 
     vector<string> tclap_argv = Heimdali::preprocess_argv(argc, argv);
     parser.parse(tclap_argv);
@@ -63,10 +68,14 @@ int main(int argc, char** argv)
 
     itk::ImageIOBase::Pointer io = Heimdali::read_informations(inputFilename0);
 
-    test_value("Number of plane", io->GetDimensions(ZD), zValue.getValue(), error_msg); 
-    test_value("Number of rows", io->GetDimensions(YD), yValue.getValue(), error_msg); 
-    test_value("Number of columns", io->GetDimensions(XD), xValue.getValue(), error_msg); 
-    test_value("Number of components", io->GetNumberOfComponents(), vValue.getValue(), error_msg); 
+    if (zValue.isSet()) test_value("Number of plane", io->GetDimensions(ZD), zValue.getValue(), error_msg); 
+    if (yValue.isSet()) test_value("Number of rows", io->GetDimensions(YD), yValue.getValue(), error_msg); 
+    if (xValue.isSet()) test_value("Number of columns", io->GetDimensions(XD), xValue.getValue(), error_msg); 
+    if (vValue.isSet()) test_value("Number of components", io->GetNumberOfComponents(), vValue.getValue(), error_msg); 
+
+    if (z0Value.isSet()) test_value("z origin", io->GetOrigin(ZD), z0Value.getValue(), error_msg);
+    if (y0Value.isSet()) test_value("y origin", io->GetOrigin(YD), y0Value.getValue(), error_msg);
+    if (x0Value.isSet()) test_value("x origin", io->GetOrigin(XD), x0Value.getValue(), error_msg);
 
     string error_msg_str = error_msg.str();
     if (error_msg.str().empty()) {
