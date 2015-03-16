c83045bbbf033984d47a8f03c6a44eaa99378a4b
David Froger <david.froger@inria.fr>
2015-03-16 11:10:49 +0100

    itest: input file names on cli

--- cmd/itest.cxx
+++ cmd/itest.cxx
@@ -1,3 +1,5 @@
+#include <sstream>
+
 #include <tclap/CmdLine.h>
 
 #include <itkINRImageIOFactory.h>
@@ -6,6 +8,8 @@
 #include "heimdali/cli.hxx"
 #include "heimdali/version.hxx"
 
+using namespace std;
+
 int main(int argc, char** argv)
 {
     try {
@@ -13,23 +17,46 @@ int main(int argc, char** argv)
     // Parse command line.
     TCLAP::CmdLine parser("Perform simple tests on image informations", ' ', HEIMDALI_VERSION);
 
+    TCLAP::UnlabeledMultiArg<string> inputFilenamesArg("inputFilenames", 
+        "Input image file name.",false,"IMAGE0-IN [IMAGE1-IN]",parser);
+
     vector<string> tclap_argv = Heimdali::preprocess_argv(argc, argv);
     parser.parse(tclap_argv);
 
     // Put our INRimage reader in the list of readers ITK knows.
     itk::ObjectFactoryBase::RegisterFactory( itk::INRImageIOFactory::New() ); 
 
+    vector<string> inputFilenames = inputFilenamesArg.getValue();
+    string inputFilename0;
+    string inputFilename1;
+
+    ostringstream error_msg;
+    switch (inputFilenames.size())
+    {
+    case 1:
+        inputFilename0 = inputFilenames[0];
+        break;
+    default:
+        error_msg << "One or two fileanmes expected, but got: "
+                  << inputFilenames.size();
+        throw(Heimdali::ValueError(error_msg.str()));
+        break;
+    }
+
+    cout << "Image informations are the same." << endl;
+
     } // End of 'try' block.
-    
 
     // Command line parser.
     catch (TCLAP::ArgException &e) { 
-        cerr << "par: ERROR: " << e.error() << " for arg " << e.argId() << endl;
+        cerr << "itest: ERROR: " << e.error() << " for arg " << e.argId() << endl;
     }
 
-
-    // Input/output.
     catch (Heimdali::IOError &e) {
-        cerr << "par: ERROR: " << e.getMessage() << endl;
+        cerr << "itest: ERROR: " << e.getMessage() << endl;
+    }
+
+    catch (Heimdali::ValueError &e) {
+        cerr << "itest: ERROR: " << e.getMessage() << endl;
     }
 }
--- tests/features/itest.feature
+++ tests/features/itest.feature
@@ -3,3 +3,11 @@ Feature: conv
     Scenario: Invoke itest
         When I run the command: itest --help
         Then I see the line in standard output: Perform simple tests on image informations 
+
+    Scenario: Compare image dimensions
+        When I run the command: itest imtest_z5_y4_x3_c2.inr
+        Then I see the standard output:
+            """
+            Image informations are the same.
+            ""
+            """
