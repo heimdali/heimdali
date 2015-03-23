a291c0b03524214e9dc4c5c366450be2ecadbe87
David Froger <david.froger@inria.fr>
2015-03-23 11:42:49 +0100

    extg supports -iv and -v
    This fix # 20

--- cmd/extg.cxx
+++ cmd/extg.cxx
@@ -12,36 +12,43 @@
 #include <itkINRImageIO.h>
 #include <itkHDF5ImageIO.h>
 #include "itkImageFileWriter.h"
+#include <itkVectorIndexSelectionCastImageFilter.h>
+#include <itkImageDuplicator.h>
+#include <itkComposeImageFilter.h>
 
 #include "itkExtractImageFilter.h"
 
 #include "heimdali/error.hxx"
+#include "heimdali/cli.hxx"
 #include "heimdali/version.hxx"
 
 using namespace std;
 
+unsigned int ZD=2, YD=1, XD=1;
+
 int main(int argc, char** argv)
 {
-    cout << scientific << showpos << setprecision(7) << uppercase;
-
     try {
 
     //////////////////////////////////////////////////////////////////////////
     // Parse comamnd line options and arguments.
     //////////////////////////////////////////////////////////////////////////
 
+    vector<string> tclap_argv = Heimdali::preprocess_argv(argc, argv);
         
     TCLAP::CmdLine cmd("Extract image subregion",' ', HEIMDALI_VERSION);
 
     // -iz -iy -ix
-    TCLAP::ValueArg<int> ixValue("k","icolumn", "First column",false,0,"IZ",cmd);
-    TCLAP::ValueArg<int> iyValue("j","irow", "First rows",false,0,"IY",cmd);
-    TCLAP::ValueArg<int> izValue("i","iplane", "First plane",false,0,"IX",cmd);
+    TCLAP::ValueArg<unsigned int> ixValue("","iz", "First column",false,0,"IZ",cmd);
+    TCLAP::ValueArg<unsigned int> iyValue("","iy", "First rows",false,0,"IY",cmd);
+    TCLAP::ValueArg<unsigned int> izValue("","ix", "First plane",false,0,"IX",cmd);
+    TCLAP::ValueArg<unsigned int> ivValue("","iv", "First component",false,0,"IV",cmd);
 
     // -z -y -x
-    TCLAP::ValueArg<int> zValue("z","nplanes", "Number of planes",false,0,"NZ", cmd);
-    TCLAP::ValueArg<int> yValue("y","nrows", "Number of rows",false,0,"NY", cmd);
-    TCLAP::ValueArg<int> xValue("x","ncolumns", "Number of columns",false,0,"NX", cmd);
+    TCLAP::ValueArg<unsigned int> zValue("z","nplanes", "Number of planes",false,0,"NZ", cmd);
+    TCLAP::ValueArg<unsigned int> yValue("y","nrows", "Number of rows",false,0,"NY", cmd);
+    TCLAP::ValueArg<unsigned int> xValue("x","ncolumns", "Number of columns",false,0,"NX", cmd);
+    TCLAP::ValueArg<unsigned int> vValue("v","ncomponent", "Number of components",false,0,"NV", cmd);
 
     // input.h5
     TCLAP::UnlabeledValueArg<string> inputFilenameArg("inputFilename", 
@@ -53,26 +60,22 @@ int main(int argc, char** argv)
         "Output image file name.",true,"output.h5","FILE-OUT");
     cmd.add(outputFilenameArg);
 
-    cmd.parse(argc,argv);
-
+    cmd.parse(tclap_argv);
 
     //////////////////////////////////////////////////////////////////////////
     // Types and instances.
     //////////////////////////////////////////////////////////////////////////
 
-
-    // Pixel
+    // Image type
     typedef float PixelType;
-
-    // Images
     const unsigned int Dimension = 3;
-    int SZ,SY,SX, NZ,NY,NX, IZ,IY,IX;
-    typedef itk::VectorImage<PixelType, Dimension> ImageType;
-    ImageType::Pointer image;
+    typedef itk::Image<PixelType, Dimension> ScalarImageType;
+    typedef itk::VectorImage<PixelType, Dimension> VectorImageType;
+    VectorImageType::Pointer image;
 
     // Readers
     itk::ObjectFactoryBase::RegisterFactory( itk::INRImageIOFactory::New() ); 
-    typedef itk::ImageFileReader< ImageType >  ReaderType;
+    typedef itk::ImageFileReader< VectorImageType >  ReaderType;
     ReaderType::Pointer reader = ReaderType::New();
 
     //////////////////////////////////////////////////////////////////////////
@@ -84,49 +87,82 @@ int main(int argc, char** argv)
     reader->Update();
 
     // Total size.
-    SZ = reader->GetImageIO()->GetDimensions(2);
-    SY = reader->GetImageIO()->GetDimensions(1);
-    SX = reader->GetImageIO()->GetDimensions(0);
+    unsigned int SZ = reader->GetImageIO()->GetDimensions(ZD);
+    unsigned int SY = reader->GetImageIO()->GetDimensions(YD);
+    unsigned int SX = reader->GetImageIO()->GetDimensions(XD);
+    unsigned int SV = reader->GetImageIO()->GetNumberOfComponents();
 
     // First index to read.
-    IZ = izValue.getValue();
-    IY = iyValue.getValue();
-    IX = ixValue.getValue();
+    unsigned int IZ = izValue.getValue();
+    unsigned int IY = iyValue.getValue();
+    unsigned int IX = ixValue.getValue();
+    unsigned int IV = ivValue.getValue();
 
     // Number of plane/column/row read.
-    NZ = zValue.getValue()==0 ? SZ-IZ : zValue.getValue();
-    NY = yValue.getValue()==0 ? SY-IY : yValue.getValue();
-    NX = xValue.getValue()==0 ? SX-IX : xValue.getValue();
+    unsigned int NZ = zValue.getValue()==0 ? SZ-IZ : zValue.getValue();
+    unsigned int NY = yValue.getValue()==0 ? SY-IY : yValue.getValue();
+    unsigned int NX = xValue.getValue()==0 ? SX-IX : xValue.getValue();
+    unsigned int NV = vValue.getValue()==0 ? SV-IV : vValue.getValue();
 
     // Create requested region.
-    ImageType::IndexType index;
-    index[2] = IZ;
-    index[1] = IY;
-    index[0] = IX;
-    ImageType::SizeType size;
-    size[2] = NZ;
-    size[1] = NY;
-    size[0] = NX;
+    VectorImageType::IndexType index;
+    index[ZD] = IZ;
+    index[YD] = IY;
+    index[XD] = IX;
+    VectorImageType::SizeType size;
+    size[ZD] = NZ;
+    size[YD] = NY;
+    size[XD] = NX;
     typedef itk::ImageRegion<Dimension> RegionType;
     RegionType region;
     region.SetIndex(index);
     region.SetSize(size);
 
-    typedef itk::ExtractImageFilter< ImageType, ImageType > ExtractFilterType;
+    typedef itk::ExtractImageFilter< VectorImageType, VectorImageType > ExtractFilterType;
     ExtractFilterType::Pointer extract = ExtractFilterType::New();
     extract->SetInput( reader->GetOutput() );
     extract->SetExtractionRegion( region );
 
+    //////////////////////////////////////////////////////////////////////////
+    // Extract components
+    //////////////////////////////////////////////////////////////////////////
+
+    // indexer
+    typedef itk::VectorIndexSelectionCastImageFilter<VectorImageType, ScalarImageType> IndexerType;
+    IndexerType::Pointer indexer = IndexerType::New();
+
+    // duplicator
+    typedef itk::ImageDuplicator<ScalarImageType> DuplicatorType;
+    typename DuplicatorType::Pointer duplicator = DuplicatorType::New();
+
+    // composer
+    typedef itk::ComposeImageFilter<ScalarImageType> ComposerType;
+    typename ComposerType::Pointer composer = ComposerType::New();
+
+    extract->Update();
+    indexer->SetInput(extract->GetOutput());
+
+    for (unsigned int iv = IV ; iv < IV+NV ; ++iv) {
+        indexer->SetIndex(iv);
+        indexer->Update();
+
+        duplicator->SetInputImage(indexer->GetOutput());
+        duplicator->Update();
+
+        composer->SetInput(iv-IV, duplicator->GetOutput());
+        composer->Modified();
+        composer->Update();
+    }
 
     //////////////////////////////////////////////////////////////////////////
     // Write image.
     //////////////////////////////////////////////////////////////////////////
 
     // Writer.
-    typedef itk::ImageFileWriter<ImageType>  WriterType;
+    typedef itk::ImageFileWriter<VectorImageType>  WriterType;
     WriterType::Pointer writer = WriterType::New();
     writer->SetFileName(outputFilenameArg.getValue());
-    writer->SetInput(extract->GetOutput());
+    writer->SetInput(composer->GetOutput());
     writer->Update();
 
     } // End of 'try' block.
--- tests/features/extg.feature
+++ tests/features/extg.feature
@@ -5,5 +5,17 @@ Feature: extg
         Then I see the line in standard output: Extract image subregion
 
     Scenario: Extract a image subregion
-        When I run the command: extg -i 1 -j 1 -k 1 -x 2 -y 2 -z 2  $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2.h5 extg_out.h5
-        Then images extg_out.h5 and $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2_i1_j1_k1_x2_y2_z2.h5 are equal
+        When I run the command: extg -ix 1 -iy 1 -iz 1 -x 2 -y 2 -z 2 $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2.<ext> imtest_z5_y4_x3_c2_extg.<ext>  
+        Then images imtest_z5_y4_x3_c2_extg.<ext> and $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2_extg.<ext> are equal
+    Examples:
+        | ext |
+        | h5  |
+        | inr |
+
+    Scenario: Extract a image subregion and component range
+        When I run the command: extg -ix 1 -iy 1 -iv 1 -iz 1 -x 2 -y 2 -z 2 -v 1 $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2.<ext> imtest_z5_y4_x3_c2_extg_v1.<ext>  
+        Then images imtest_z5_y4_x3_c2_extg_v1.<ext> and $HEIMDALI_DATA_DIR/imtest_z5_y4_x3_c2_extg_v1.<ext> are equal
+    Examples:
+        | ext |
+        | h5  |
+        | inr |
